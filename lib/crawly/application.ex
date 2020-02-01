ed9bc632190d44a67beac601e0de4e5bc2ba8faa
ogabriel <gabrieloliver8991@gmail.com>
2020-02-01 16:41:17 -0200

    add port to ranch

--- lib/crawly/application.ex
+++ lib/crawly/application.ex
@@ -19,7 +19,9 @@ defmodule Crawly.Application do
       {DynamicSupervisor,
        strategy: :one_for_one, name: Crawly.DataStorage.WorkersSup},
       {Plug.Cowboy,
-       scheme: :http, plug: Crawly.API.Router, options: [port: 4001]}
+       scheme: :http,
+       plug: Crawly.API.Router,
+       options: [port: Application.get_env(:crawly, :port, 4001)]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
