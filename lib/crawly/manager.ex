71957cb4cab84d3776b4213f4dc6c4707430e547
ogabriel <gabrieloliver8991@gmail.com>
2020-02-23 19:01:23 -0300

    dont stop crawler

--- lib/crawly/manager.ex
+++ lib/crawly/manager.ex
@@ -85,34 +85,6 @@ defmodule Crawly.Manager do
     delta = items_count - state.prev_scraped_cnt
     Logger.info("Current crawl speed is: #{delta} items/min")
 
-    case Application.get_env(:crawly, :closespider_itemcount, 1000) do
-      cnt when cnt < items_count ->
-        Logger.info(
-          "Stopping #{inspect(state.name)}, closespider_itemcount achieved"
-        )
-
-        Crawly.Engine.stop_spider(state.name)
-
-      _ ->
-        :ignoring
-    end
-
-    # Close spider in case if it's not scraping itms fast enough
-    case Application.get_env(:crawly, :closespider_timeout) do
-      :undefined ->
-        :ignoring
-
-      cnt when cnt > delta ->
-        Logger.info(
-          "Stopping #{inspect(state.name)}, itemcount timeout achieved"
-        )
-
-        Crawly.Engine.stop_spider(state.name)
-
-      _ ->
-        :ignoring
-    end
-
     tref = Process.send_after(self(), :operations, get_timeout())
 
     {:noreply, %{state | tref: tref, prev_scraped_cnt: items_count}}
